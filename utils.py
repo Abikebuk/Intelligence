3a3fa6c1a6e8f64ae10f1bad01fd095bba1676f3
rettana <rettana.muon@gmail.com>
2024-06-23 13:23:10 +0200

    add utils.py with file management functions

---
+++ utils.py
@@ -0,0 +1,24 @@
+import os
+import pathlib
+import pickle
+
+
+def load_pickle(path):
+    if os.path.exists(path):
+        print(f"Loading pickle from {path}...")
+        with open(path, "rb") as f:
+            return pickle.load(f)
+    else:
+        print("File not found.")
+        return None
+
+
+def save_pickle(path, obj):
+    create_dirs(path)
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
+        print(f"Saved pickle to {path}...")
+
+
+def create_dirs(file_path: str):
+    pathlib.Path('/my/directory').mkdir(parents=True, exist_ok=True)
